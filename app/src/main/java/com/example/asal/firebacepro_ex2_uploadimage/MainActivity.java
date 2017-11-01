3bfd80e4677d012063f38e2f476381ae7db544d4
asal <asal.rostami1984@gmail.com>
2017-11-01 10:17:19 -0400

    Singing And Signout firebase

--- app/src/main/java/com/example/asal/firebacepro_ex2_uploadimage/MainActivity.java
+++ app/src/main/java/com/example/asal/firebacepro_ex2_uploadimage/MainActivity.java
@@ -1,24 +1,88 @@
 package com.example.asal.firebacepro_ex2_uploadimage;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
     private EditText mEditTextEmail,mEditTextPassword;
     private Button mButtonSignin,mButtonSignout;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Toast.makeText(MainActivity.this,"successfully signed in",Toast.LENGTH_SHORT).show();
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Toast.makeText(MainActivity.this,"successfully signed out",Toast.LENGTH_SHORT).show();
+                }
+                // ...
+            }
+        };
         mEditTextEmail = (EditText)findViewById(R.id.editTextEmail);
         mEditTextPassword = (EditText)findViewById(R.id.editTextPassword);
 
         mButtonSignin = (Button)findViewById(R.id.btnSignin);
+        mButtonSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEditTextEmail.getText().toString();
+                String password = mEditTextPassword.getText().toString();
+                if(!email.equals("") && !password.equals(""))
+                {
+                    mAuth.signInWithEmailAndPassword(email,password);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Please Enter Information",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         mButtonSignout = (Button)findViewById(R.id.btnSignout);
+        mButtonSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Toast.makeText(MainActivity.this,"Signing out....",Toast.LENGTH_SHORT).show();
+            }
+        });
 
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
